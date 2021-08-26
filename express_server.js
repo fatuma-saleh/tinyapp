afaebc4aa4675d8e8a8f16b9a8570b4abfd1ead2
Fatuma <sfatex@gmail.com>
2021-08-26 17:28:08 -0400

    Save key-value pair  to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -61,8 +61,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  //console.log("++++",req.body);  // Log the POST request body to the console
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  res.redirect(`urls/${shortURL}`)
 });
 
 app.listen(PORT, () => {
